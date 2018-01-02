02d8d50bfc0e7857db330d3033244b409c5ef5e4
mmallahham <mmallahham@hotmail.com>
2018-01-02 12:49:52 +0100

    test mongobd

--- mongo.js
+++ mongo.js
@@ -1,8 +1,13 @@
 var MongoClient = require('mongodb').MongoClient;
-
-var uri = "mongodb+srv://mmallahham:MMAmma123@cluster0-xrxwg.mongodb.net/library";
-MongoClient.connect(uri, function(err, db) {
-    console.log(db);
-  //var collection = db.collection('books');
+//var url = "mongodb://localhost:27017/library";
+var url = "mongodb+srv://mmallahham:MMAmma123@cluster0-xrxwg.mongodb.net/myDB" ;
+MongoClient.connect(url, function(err, db) {
+  
+  var dbase = db.db('myDB');
+  const collection = dbase.collection('books');
+  collection.insertMany([{"Auther":"Not me"}],(err,resualt)=>{
+    console.log(err);
+  });
+  console.log(collection);
   db.close();
 });
