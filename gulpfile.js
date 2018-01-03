e6784113f6d57688ee2fb7c8763d59112efa60af
mmallahham <mmallahham@hotmail.com>
2018-01-03 16:00:00 +0100

    fix some error

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,24 @@
 var gulp = require("gulp");
 var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
+var babel = require('gulp-babel')
 
 var jsfiles = ['*.js','src/**/*.js'];
+
+gulp.task('default',function(){
+    nodemon({
+        script: 'buildScripts/srcServer.js',
+        ext: 'js',
+        env:{
+            port:8000
+        },
+        ignore: ['./node_modules/**']
+    })
+    .on('restart',()=>{
+        console.log('Server is restarting...');
+    });
+});
+
 
 gulp.task('style',function(){
     gulp.src(jsfiles).pipe(jshint());
